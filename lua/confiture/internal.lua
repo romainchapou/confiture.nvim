27472c908e9757837c7078a54daa74fafa7184c4
Romain Chapoullié <romain.chapoullie@grenoble-inp.org>
2022-08-28 23:43:24 +0200

    Add command substitution and refacto

--- lua/confiture/internal.lua
+++ lua/confiture/internal.lua
@@ -1,14 +1,36 @@
 local internal = {}
 local utils = require("confiture.utils")
 
-local function replace_variables_in_string(val_str, variables, line_nb)
+local function replace_commands_in_string(val_str, commands, err)
   local ret_string = val_str
 
-  for to_replace in string.gmatch(val_str, "%${([%w_]+)}") do
-    if variables[to_replace] ~= nil then
-      ret_string = string.gsub(ret_string, "${" .. to_replace .. "}", "\"" .. variables[to_replace] .. "\"")
+  for to_replace in string.gmatch(val_str, "@{([%w_]*)}") do
+    if commands[to_replace] ~= nil then
+      -- don't add quotes here, paste the command directly
+      ret_string = string.gsub(ret_string, "@{" .. to_replace .. "}", commands[to_replace])
     else
-      utils.warn('Failed to replace undeclared variable "' .. to_replace .. '" in line' .. line_nb)
+      err.msg = 'Failed to replace undeclared command "@' .. to_replace .. '"'
+      return
+    end
+  end
+
+  return ret_string
+end
+
+local function replace_variables_in_string(val_str, variables, err)
+  local ret_string = val_str
+
+  for to_replace in string.gmatch(val_str, "%${([%w_]*)}") do
+    if variables[to_replace] ~= nil then
+      if type(variables[to_replace]) == "string" then
+        -- add quotes here in case of spaces
+        ret_string = string.gsub(ret_string, "${" .. to_replace .. "}", "\"" .. variables[to_replace] .. "\"")
+      else
+        err.msg = 'Failed to replace ' .. type(variables[to_replace]) .. ' variable "' .. to_replace .. '"'
+        return nil
+      end
+    else
+      err.msg = 'Failed to replace undeclared variable "' .. to_replace .. '"'
       return nil
     end
   end
@@ -18,74 +40,69 @@ local function replace_variables_in_string(val_str, variables, line_nb)
   return ret_string
 end
 
+
 -- A valid command definition should be of the form:
---      @command_name : [@first_dep_cmd @second_dep_cmd ...] <"script to run">
+--      @command_name : "script to run"
 --
--- * any number of dependent commands possible, including 0
--- * if one or more dependent command, having a script to run is optional
--- * if no dependent command, having a script to run is required
-local function parse_command(tokens, state_to_update, line_nb)
-  if #tokens <= 2 then
-    return "incomplete command definition, missing value"
-  end
-
-  local command = {
-    name = tokens[1].value,
-    dependencies = {},
-    script = nil
-  }
-
-  local i = 3
-  while tokens[i] and tokens[i].type == "command" do
-    table.insert(command.dependencies, tokens[i].value)
-    i = i + 1
-  end
-
-  if tokens[i] and tokens[i].type == "script_str" then
-    command.script = tokens[i].value
-    i = i + 1
-  end
-
-  if tokens[i] then
-    return "invalid extra token '" .. tokens[i].value .. "' of type " .. tokens[i].type
-  end
-
-  command.script = replace_variables_in_string(command.script, state_to_update.variables, line_nb)
-
-  -- TODO @Incomplete take dependencies into account
-  state_to_update.commands[command.name] = command.script
-end
-
 -- A valid variable definition should be of the form:
 --      variable_name : "string value"
 -- or:
 --      variable_name : boolean_value
-local function parse_variable(tokens, state_to_update, line_nb)
-  if #tokens <= 2 then
-    return "incomplete variable definition, missing value"
+local function parse_command_or_var_and_add_to_state(tokens, state_to_update)
+  local first_token_type = tokens[1].type
+
+  if first_token_type ~= "command" and first_token_type ~= "variable" then
+    return "not a command or variable definition (first token of type " .. first_token_type
   end
 
-  local variable = {
+  if not tokens[2] or tokens[2].type ~= "separator" then
+    return "not a command or variable definition, missing ':' separator"
+  end
+
+  if #tokens <= 2 then
+    return "incomplete " .. first_token_type .. " definition, missing value"
+  end
+
+  local output = {
     name = tokens[1].value,
     value = nil
   }
 
   local i = 3
-  if tokens[i] and tokens[i].type == "script_str" or tokens[i].type == "boolean" then
-    variable.value = tokens[i].value
+  if tokens[i] and tokens[i].type == "script_str" or
+    (first_token_type == "variable" and tokens[i].type == "boolean") then
+    output.value = tokens[i].value
     i = i + 1
+  elseif tokens[i] and first_token_type == "variable" then
+    return "variable value should be a string or a boolean"
+  elseif tokens[i] and first_token_type == "command" then
+    return "command value should be a script string"
+  else
+    return "incomplete variable definition, missing value"
   end
 
   if tokens[i] then
     return "invalid extra token '" .. tokens[i].value .. "' of type " .. tokens[i].type
   end
 
-  if type(variable.value) == "string" then
-    variable.value = replace_variables_in_string(variable.value, state_to_update.variables, line_nb)
+  if type(output.value) == "string" then
+    local err = {}
+
+    output.value = replace_commands_in_string(output.value, state_to_update.commands, err)
+    if err.msg then return err.msg end
+    output.value = replace_variables_in_string(output.value, state_to_update.variables, err)
+    if err.msg then return err.msg end
   end
 
-  state_to_update.variables[variable.name] = variable.value
+  if first_token_type == "variable" then
+    state_to_update.variables[output.name] = output.value
+  elseif first_token_type == "command" then
+    state_to_update.commands[output.name] = output.value
+  else
+    utils.warn("This should never happen")
+  end
 end
+
 
 local function check_boolean_variable_val(state, var)
   if type(state.variables[var]) ~= "boolean" then
@@ -101,29 +118,6 @@ local function warn_parsing(line_nb, err_msg)
   utils.warn('Configuration file error line ' .. line_nb .. ': ' .. err_msg)
 end
 
-local function add_tokenized_line_infos_to_state(parsed_tokens, state, line_nb)
-  local first_token_type = parsed_tokens[1].type
-
-  local err_msg
-
-  if first_token_type ~= "command" and first_token_type ~= "variable" then
-    err_msg = "not a command or variable definition (first token of type " .. first_token_type
-    return err_msg
-  end
-
-  if not parsed_tokens[2] or parsed_tokens[2].type ~= "separator" then
-    err_msg =  "not a command or variable definition, missing ':' separator"
-    return err_msg
-  end
-
-  if first_token_type == "command" then
-    err_msg = parse_command(parsed_tokens, state, line_nb)
-  else -- first_token_type == "variable"
-    err_msg = parse_variable(parsed_tokens, state, line_nb)
-  end
-
-  return err_msg
-end
 
 -- 'config_file' supposed to exist
 function internal.read_configuration_file(config_file)
@@ -151,7 +145,7 @@ function internal.read_configuration_file(config_file)
     end
 
     if #parsed_tokens ~= 0 then -- skip empty/comment lines
-      local err_msg = add_tokenized_line_infos_to_state(parsed_tokens, state, line_nb)
+      local err_msg = parse_command_or_var_and_add_to_state(parsed_tokens, state)
 
       if err_msg then
         warn_parsing(line_nb, err_msg)
